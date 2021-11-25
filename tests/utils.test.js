becb3e62b26ca65ddb07663c7a660f30d1ac6b22
ctime-zliang <1156282805@qq.com>
2021-11-25 18:11:13 +0800

    feat: modify - daily commit

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -4,4 +4,7 @@ describe(`Utils Functions Test`, () => {
 	test('2 ** 3 = 8', () => {
 		expect(powerValue(2, 3)).toBe(8)
 	})
+	test('0.1 + 0.2 = 0.3', () => {
+		expect( 0.1 + 0.2).toBeCloseTo(0.3)
+	})
 })
