2bbb1a44721d9f0cfc31c9a469b92ff8dfeebbb4
ctime-zliang <1156282805@qq.com>
2021-12-13 09:29:52 +0800

    feat: modify - change puppeteer config about get-file-dir

--- config/jest-puppeteer/get.runtime.filedir.js
+++ config/jest-puppeteer/get.runtime.filedir.js
@@ -2,5 +2,5 @@ const fs = require('fs')
 const path = require('path')
 
 module.exports = () => {
-	return path.join(process.cwd() + '/.puppeteer.runtime/', 'jest_puppeteer_global_setup')
+	return path.join(process.cwd() + '/.puppeteer/', 'jest_puppeteer_global_setup')
 }
