09dc81fa89b01295926caaf178a1796da0a44be3
ctime-zliang <1156282805@qq.com>
2022-09-29 09:17:08 +0800

    feat: modify - daily commit

--- src/store/valtio/Hooks.ts
+++ src/store/valtio/Hooks.ts
@@ -2,7 +2,7 @@ import { proxy, useSnapshot } from 'valtio'
 import { valtioStore } from './store'
 
 export const useComparisonThreshold = (): boolean => {
-	const threshold: number = 3
+	const THRESHOLD: number = 3
 	const valtioStoreSnapshot = useSnapshot(valtioStore)
-	return valtioStoreSnapshot.counter >= threshold
+	return valtioStoreSnapshot.counter >= THRESHOLD
 }
