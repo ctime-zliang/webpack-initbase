c4bf77a768aedbda4a015c2fe7d181d1e4682676
ctime-zliang <1156282805@qq.com>
2022-10-13 11:10:14 +0800

    feat: modify - add comments

--- src/app/store/redux.ts
+++ src/app/store/redux.ts
@@ -7,33 +7,41 @@ import { moduleKey as globalDefault_moduleKey } from './globalDefault/config'
 
 const createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore)
 
+/**
+ * 创建 reducer
+ *      创建由静态 reducer 和动态传入的 async-reducer 组成的 reducers 对象
+ */
 function createCombineReducers(asyncReducers: { [key: string]: any } = {}) {
 	const o: { [key: string]: any } = {
 		[globalDefault_moduleKey]: globalDefault_createReducer(),
 		...asyncReducers,
 	}
-	return {
-		combinedReducer: combineReducers(o),
-		syncReducerKeys: Object.keys(o),
-	}
+	return combineReducers(o)
 }
 
+/**
+ * 动态注入异步 reducer
+ *      在注册路由的时候注册 reducer
+ */
 export function modulesInjectReducer(store: TReduxStore, key: string, createAsyncReducer: Function): TReduxStore {
 	const concernedStoreKeys: Array<string> = Object.keys(store.asyncReducers || {})
 	if (concernedStoreKeys.includes(key)) {
 		return store
 	}
 	store.asyncReducers[key] = createAsyncReducer()
-	const ccr = createCombineReducers(store.asyncReducers)
-	store.replaceReducer(ccr.combinedReducer)
+	const combineReducers = createCombineReducers(store.asyncReducers)
+	store.replaceReducer(combineReducers)
 	return store
 }
 
 export const configureStore = (): TReduxStore => {
-	const ccr = createCombineReducers()
-	const store = createStoreWithMiddleware(ccr.combinedReducer, {
+	const combineReducers = createCombineReducers()
+	const store = createStoreWithMiddleware(combineReducers, {
 		[globalDefault_moduleKey as string]: globalDefault_initialState,
 	}) as TReduxStore
+	/**
+	 * 记录已经注册过的动态异步 reducer
+	 */
 	store.asyncReducers = {}
 	return store
 }
