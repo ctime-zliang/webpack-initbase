5c70d8cfedc238b6d97492f9c6468dc913e9f4f3
ctime-zliang <1156282805@qq.com>
2022-11-09 12:01:58 +0800

    feat: modify - daily commit

--- srcReact/app/utils/hooks/useDervStore/hook.ts
+++ srcReact/app/utils/hooks/useDervStore/hook.ts
@@ -22,9 +22,6 @@ function createEffectHandler(
 		store.listeners.push(newListenerItem)
 	}
 	return (): void => {
-		/**
-		 * 从 store.listeners 中移除指定的 listener
-		 */
 		store.listeners = (store.listeners as Array<TListenerEffectItem>).filter((item: TListenerEffectItem): boolean => {
 			return item !== newListenerItem
 		})
